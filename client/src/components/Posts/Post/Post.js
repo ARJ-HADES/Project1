55d042a7c0784bd370b4058f0ef9b0ee6d670d7b
ARJ-HADES <arjunvinod161@gmail.com>
2022-05-20 12:00:39 +0400

    commit on part1

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import useStyles from './styles'
 import {Card, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core"
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
+import useStyles from './styles'
+import {useDispatch} from 'react-redux'
+
+import {deletePost} from '../../../actions/posts'
      
 const Post =({post, setCurrentId}) =>{
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+
     return(
         <Card className ={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title ={post.title}/>
@@ -33,7 +39,7 @@ const Post =({post, setCurrentId}) =>{
                             Like
                             {post.likeCount}
                         </Button>
-                        <Button size="small" color ="primary" onClick={()=>{}}>
+                        <Button size="small" color ="primary" onClick={()=> dispatch(deletePost(post._id))}>
                             <DeleteIcon fontSize='small'/>
                             Delete
                         </Button>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,24 +29,36 @@ import PostMessage from '../models/postMessage.js'
 
          
 // https://www.restapitutorial.com/httpstatuscodes.html
-         res.status(201).jason(newPost)
+         res.status(201).json(newPost)
          
      } catch (error) {
-         res.status(401).jason({message: error.message})
+         res.status(401).json({message: error.message})
          
      }
  }
 
 
- export const updatePost = async (res, req) =>{
+ export const updatePost = async (req, res) =>{
      const {id : _id} = req.params
      const post =req.body
 
      if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('no post with that id')
 
      
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {... post, _id}, {new :true}) 
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new :true}) 
 
-     res.jason(updatedPost)
+     res.json(updatedPost)
+
+ }
+
+ export const deletePost = async(req, res) => {
+     const{id} = req.params
+     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('no post with that id')
+
+     await PostMessage.findByIdAndRemove(id)
+      
+     
+
+     res.json({message :'Post deleted successfully'})
 
  }
