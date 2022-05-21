d6061fa452d221a9f22e9ea3386ef6941df58aef
ARJ-HADES <arjunvinod161@gmail.com>
2022-05-21 13:49:19 +0400

    commit on part2

--- server/index.js
+++ server/index.js
@@ -18,8 +18,7 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes)
-// const CONNECTION_URL =
-// "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.egow6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, {
 useNewUrlParser: true,
