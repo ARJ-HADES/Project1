599d2ec6c9fa149c0001475eb42081a57ca39e84
ARJ-HADES <arjunvinod161@gmail.com>
2022-05-21 13:07:56 +0400

    commit on part2

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from 'express'
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 
 const app = express();
-
+dotenv.config()
 
 
 
@@ -18,10 +18,10 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes)
-const CONNECTION_URL =
-"mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.egow6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+// "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.egow6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.CONNECTION_URL, {
 useNewUrlParser: true,
 useUnifiedTopology: true
 // useCreateIndex: true
