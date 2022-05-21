c35109ce7790622bba9e1bdb5e37350e6ddc8557
ARJ-HADES <arjunvinod161@gmail.com>
2022-05-21 14:04:17 +0400

    commit on part3

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, {useState, useEffect} from 'react'
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import {Container, Grow, Grid} from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 
 
 import {getPosts} from './actions/posts'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
-import memories from './images/memories.png'
+import Navbar from './components/Navbar/Navbar' 
 import useStyles from './styles'
+
 const App = ()=>{
     const [currentId, setCurrentId] =useState(null)
     const classes = useStyles()
@@ -19,12 +20,7 @@ const App = ()=>{
 
     return(
         <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">
-                    Memories
-                </Typography>
-             <img className={classes.image} src={memories} alt="memories" height="60"/>
-            </AppBar>
+            <Navbar/>
             <Grow in>
                 <Container>
                     <Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3}>
