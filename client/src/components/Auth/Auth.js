b071790a25a04daadbdb5a2c1470081b679db800
ARJ-HADES <arjunvinod161@gmail.com>
2022-05-24 16:53:45 +0400

    commit on part3

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Typography, Avatar, Paper, Container, Grid} from '@material-ui/core'
+import {GoogleLogin} from 'react-google-login'
+
+import Icon from './Icon'
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
 import useStyles from './Styles'
 import Input from "./Input"
@@ -7,8 +10,12 @@ import Input from "./Input"
 
 const Auth = () => {
    const classes = useStyles()
+   const [showPassword, setShowPassword] = useState(false)
+   const [isSignup, setIsSignup] = useState(false)
+   
 
-   const isSignup = false
+   const  handleShowPassword =() => setShowPassword((prevShowPassword)=> !prevShowPassword)
+
    const handleSubmit =() =>{
 
    }
@@ -16,6 +23,19 @@ const Auth = () => {
    const handleChange =() =>{
 
    }
+   const switchMode =() =>{
+        setIsSignup((prevIsSignup)=> !prevIsSignup)
+        handleShowPassword (false)
+   }
+
+        const googleSuccess = async (res) => {
+            console.log(res)
+        }
+        const googleFailure = () => {
+            
+            console.log("Google Sign In was unsuccessful. Try Again Later ")
+        }
+         
 
   return (
       <Container component="main" maxWidth='xs'>
@@ -30,10 +50,41 @@ const Auth = () => {
                         isSignup && (
                             <>
                                 <Input name='FirstName' label="First name" handleChange={handleChange} autoFocus half />
-                                <Input name='LastName' label="last name" handleChange={handleChange}  half />                                                           
+                                <Input name='LastName' label="Last name" handleChange={handleChange}  half />                                                           
                             </>
                         )
                     }
+                    <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                    <Input name='password' label='Password' autocomplete="on" handleChange={handleChange} type= {showPassword ? "text" : "password" } handleShowPassword={handleShowPassword} autoFocus />
+                    {isSignup && <Input name= 'confirmPassword' label= 'Repeat Password' handleChange={handleChange} type= 'password' />}
+                </Grid> 
+                <Button  type='submit' fullWidth variant ='contained' color ='primary' className={classes.submit} >
+                    {isSignup ?  'Sign Up' : 'Sign In'}
+                </Button>
+                <GoogleLogin
+                            clientId ="322027900789-rumalvf2kmikp0115c6gk12f8gqrit63.apps.googleusercontent.com" 
+                            render={(renderProps)=>(
+                                <Button 
+                                    className={classes.googleButton} 
+                                    color="primary" 
+                                    fullWidth 
+                                    onClick={renderProps.onClick} 
+                                    disabled = {renderProps.disabled} 
+                                    startIcon={<Icon/>} 
+                                    variant ="contained">
+                                        Google Sign In
+                                </Button>
+                            )}
+                            onSuccess={googleSuccess}
+                            onFailure={googleFailure}
+                            cookiePolicy = "single_host_origin"
+                        />
+                <Grid container justifyContent = 'flex-end'>
+                    <Grid item>                        
+                        <Button onClick= {switchMode}>
+                            {isSignup ? "Already have an account? Sign In " : " Don't have an account? Sign Up"}
+                        </Button>
+                    </Grid>
 
                 </Grid>
             </form>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -31,7 +31,7 @@ const Post =({post, setCurrentId}) =>{
                 </div>
                     <Typography className={classes.title} variant= "h5" gutterBottom >{post.title}</Typography>
                     <CardContent>
-                          <Typography variant= "body2" color="text secondary" component="p">{post.message}</Typography>
+                          <Typography variant= "body2" color="textSecondary" component="p">{post.message}</Typography>
                     </CardContent>
                     <CardActions className={classes.cardActions}>
                         <Button size="small" color ="primary" onClick={()=> dispatch(likePost(post._id))}>
