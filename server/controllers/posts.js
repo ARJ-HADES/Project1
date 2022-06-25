575000dd605fa6a4f44d1e174a818a3946d8e065
ARJ-HADES <68515819+ARJ-HADES@users.noreply.github.com>
2022-06-25 17:19:44 +0400

    commit on master

---
+++ server/controllers/posts.js
@@ -0,0 +1,106 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  const {page} =req.query
+
+  try {
+    const LIMIT = 4
+    const startIndex = (Number(page)-1)*LIMIT //Get the starting index of every page 
+    const total = await PostMessage .countDocuments({})
+    const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
+
+    res.status(200).json({data:posts, currentPage: Number(page), numberOfPages: Math.ceil(total/LIMIT)});
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getPostsBySearch = async(req, res)=>{
+  const {searchQuery, tags} = req.query
+  try {
+    const title = new RegExp(searchQuery, 'i')
+
+    const posts = await PostMessage.find({ $or:[{title},{tages: { $in: tags.split(',') }}] }) 
+
+    res.json({data: posts})
+  } catch (error) {
+    res.status(404).json({message: error.message})
+  }
+}
+
+export const getPost = async (req,res) => {
+  const {id} =req.params
+  try {
+    const post  = await PostMessage.findById(id)
+    
+    res.status(200).json(post)
+  } catch (error) {
+    res.status(404).json({message: error.message})
+  }
+}
+
+
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()});
+
+  try {
+    await newPost.save();
+
+    // https://www.restapitutorial.com/httpstatuscodes.html
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("no post with that id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("no post with that id");
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully" });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!req.userId) return res.json({ message: "Unauthenticated" });
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("no post with that id");
+
+  const post = await PostMessage.findById(id);
+
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
